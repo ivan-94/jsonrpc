92cdd53c005796d36b900e0b0a016ef0ca45768a
carney520 <carney520@hotmail.com>
2020-03-11 17:46:47 +0800

    feat: 新增错误类型区分

--- src/index.tsx
+++ src/index.tsx
@@ -14,8 +14,11 @@ import compose from './compose'
 import extraResult from './interceptors/extraResult'
 
 export * from './type'
-export const ErrJsonParse = 0
+
+export const ErrNetwork = 0
+export const ErrJsonParse = 1
 export const ErrIDNotMatching = 2
+export const ErrCancel = 3
 
 export interface ClientOptions {
   // 忽略协议错误
@@ -33,6 +36,63 @@ function createError<P, R>(
   error.request = request
   error.response = response
   return error
+}
+
+export function isRequestError<P, R>(err: any): err is RequestError<P, R> {
+  return (
+    err != null && typeof err === 'object' && 'request' in err && 'code' in err
+  )
+}
+
+const supportCheckOnline = 'onLine' in navigator
+
+export enum ErrorType {
+  Unknown,
+  Cancel,
+  Network,
+  Client,
+  Server,
+}
+
+/**
+ * 创建取消信号
+ * @param req
+ */
+export function createCancelError(req: JSONRPCRequest<any>) {
+  return createError('cancelled', ErrCancel, req)
+}
+
+export function isCancelled(err: any) {
+  return err && err.code === ErrCancel
+}
+
+export function isNetworkError(err: any) {
+  return err && err.code === ErrNetwork
+}
+
+/**
+ * 我们约定，400 - 500(不包含) 错误码范围的为客户端本身的错误
+ * @param err
+ */
+export function isClientError(err: any) {
+  return err && typeof err.code === 'number' && err.code >= 400 && err < 500
+}
+
+// 除上述错误类型之外的，都属于服务端错误
+export function isServerError(err: any) {
+  return !isCancelled(err) && !isNetworkError(err) && !isClientError(err)
+}
+
+export function getErrorType(err: any) {
+  return isRequestError(err)
+    ? isNetworkError(err)
+      ? ErrorType.Network
+      : isCancelled(err)
+        ? ErrorType.Cancel
+        : isClientError(err)
+          ? ErrorType.Client
+          : ErrorType.Server
+    : ErrorType.Unknown
 }
 
 export default class JSONRPC {
@@ -75,16 +135,27 @@ export default class JSONRPC {
     request: JSONRPCRequest<P>,
     shouldExtraResult: boolean = false,
   ): Promise<R> {
+    // 拦截检查是否网路错误
+    if (supportCheckOnline && !navigator.onLine) {
+      return Promise.reject(
+        createError(this.networkErrorMessage, ErrNetwork, request),
+      )
+    }
+
     const req = new XMLHttpRequest()
     let url = this.root
+
     // 可以在控制台直接显示请求的方法
     const query = `_m=${request.method}`
+
     if (url.indexOf('?') === -1) {
       url += '?' + query
     } else {
       url += '&' + query
     }
+
     req.open('POST', url)
+
     const interceptors = shouldExtraResult
       ? [...this.preInterceptor, ...this.interceptor]
       : this.interceptor
@@ -124,11 +195,12 @@ export default class JSONRPC {
                 resolve(resp)
               }
             } catch (error) {
-              reject(createError(error.message, 0, request))
+              reject(createError(error.message, ErrJsonParse, request))
             }
           } else if (req.readyState === XMLHttpRequest.DONE) {
             // 网络异常
             const error = createError(
+              // 网络异常时， req.status 为 0
               req.status === 0 ? this.networkErrorMessage : req.statusText,
               req.status,
               request,
